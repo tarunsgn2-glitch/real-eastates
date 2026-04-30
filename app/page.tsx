54df337e5808381d37602cf267f0b75742fac48d
tarunsgn2-glitch <tarunsgn2@gmail.com>
2026-04-30 20:03:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
-import Navbar from '../components/Navbar';
-import PropertyCard from '../components/PropertyCard';
+import navbar from '../components/navbar';
+import propertycard from '../components/propertycard';
 import Link from 'next/link';
 
 export default function Home() {
